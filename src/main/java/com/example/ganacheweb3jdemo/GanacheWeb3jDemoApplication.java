5b107be310bb9e69ab088d877ab4bbf7ed01092d
lykismekk <lykismekk@outlook.com>
2022-03-30 23:03:03 +0800

    Initial commit

---
+++ src/main/java/com/example/ganacheweb3jdemo/GanacheWeb3jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ganacheweb3jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanacheWeb3jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GanacheWeb3jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ganacheweb3jdemo/GanacheWeb3jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ganacheweb3jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanacheWeb3jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
