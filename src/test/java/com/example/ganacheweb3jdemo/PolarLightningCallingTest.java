4b28f728b377d758811faad9b289e2c419ccd4bb
Roylic <roylic@wokoworks.com>
2022-05-24 19:33:47 +0800

    tmp commit for macaroon

--- src/test/java/com/example/ganacheweb3jdemo/PolarLightningCallingTest.java
+++ src/test/java/com/example/ganacheweb3jdemo/PolarLightningCallingTest.java
@@ -1,17 +1,19 @@
 package com.example.ganacheweb3jdemo;
 
-
 import com.example.ganacheweb3jdemo.web3j.ApplicationInterceptorImp;
 import com.example.ganacheweb3jdemo.web3j.LogInterceptorImp;
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
+import com.github.nitram509.jmacaroons.Macaroon;
+import com.github.nitram509.jmacaroons.MacaroonsBuilder;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import org.junit.jupiter.api.Test;
-import org.lightningj.lnd.wrapper.ClientSideException;
-import org.lightningj.lnd.wrapper.StatusException;
-import org.lightningj.lnd.wrapper.SynchronousLndAPI;
-import org.lightningj.lnd.wrapper.ValidationException;
+import org.lightningj.lnd.wrapper.*;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
@@ -36,9 +38,21 @@ public class PolarLightningCallingTest {
     private final static String DAVE_CERT = POLAR_MACAROON_LOC + "/dave/tls.cert";
     private final static String DAVE_MACAROON = POLAR_MACAROON_LOC + "/dave/data/chain/bitcoin/regtest/admin.macaroon";
 
+    // Bob - c-lightning
+    private final static String BOB_REST_API = "http://127.0.0.1:8182";
+    private final static String BOB_MACAROON = "/Users/pundix2022/.polar/networks/1/volumes/c-lightning/bob/rest-api/access.macaroon";
+    private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(new ApplicationInterceptorImp())
+            .addNetworkInterceptor(new LogInterceptorImp())
+            .retryOnConnectionFailure(false)
+            .build();
+
 
     @Test
-    public void syncChannelBalanceTest() throws IOException, StatusException, ValidationException {
+    public void LND_syncChannelBalanceTest() throws IOException, StatusException, ValidationException {
 
         SynchronousLndAPI synchronousLndAPI = new SynchronousLndAPI(
                 "127.0.0.1",
@@ -47,6 +61,24 @@ public class PolarLightningCallingTest {
                 new File(ALICE_MACAROON));
 
         System.out.println(synchronousLndAPI.channelBalance().toJsonAsString(true));
+    }
+
+    @Test
+    public void CLIGHTNING_syncChannelBalanceTest() throws ClientSideException, FileNotFoundException {
+
+        File file = new File(BOB_MACAROON);
+        StaticFileMacaroonContext context = new StaticFileMacaroonContext(file);
+        String currentMacaroonAsHex = context.getCurrentMacaroonAsHex();
+
+
+//        Request requestGet = new Request.Builder()
+//                .url(BOB_REST_API + "/v1/getinfo")
+//                .header("macaroon", currentMacaroonAsHex)
+//                .build();
+//
+//        Call call = okHttpClient.newCall(requestGet);
+//        RequestBody body = call.request().body();
+//        System.out.println();
 
     }
 
