9cdd6a2b44063240cfcad8fabb435052d6aaa1b7
Roylic <roylic@wokoworks.com>
2022-05-25 11:02:10 +0800

    successfully request to c-lightning node on Polar

--- src/test/java/com/example/ganacheweb3jdemo/PolarLightningCallingTest.java
+++ src/test/java/com/example/ganacheweb3jdemo/PolarLightningCallingTest.java
@@ -4,17 +4,16 @@ import com.example.ganacheweb3jdemo.web3j.ApplicationInterceptorImp;
 import com.example.ganacheweb3jdemo.web3j.LogInterceptorImp;
 import com.github.nitram509.jmacaroons.Macaroon;
 import com.github.nitram509.jmacaroons.MacaroonsBuilder;
-import okhttp3.Call;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
+import okhttp3.*;
+import org.bouncycastle.util.encoders.Base64;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.lightningj.lnd.wrapper.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -39,7 +38,7 @@ public class PolarLightningCallingTest {
     private final static String DAVE_MACAROON = POLAR_MACAROON_LOC + "/dave/data/chain/bitcoin/regtest/admin.macaroon";
 
     // Bob - c-lightning
-    private final static String BOB_REST_API = "http://127.0.0.1:8182";
+    private final static String BOB_REST_API = "http://127.0.0.1:8182/";
     private final static String BOB_MACAROON = "/Users/pundix2022/.polar/networks/1/volumes/c-lightning/bob/rest-api/access.macaroon";
     private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .readTimeout(10, TimeUnit.SECONDS)
@@ -64,23 +63,34 @@ public class PolarLightningCallingTest {
     }
 
     @Test
-    public void CLIGHTNING_syncChannelBalanceTest() throws ClientSideException, FileNotFoundException {
+    public void CLIGHTNING_syncChannelBalanceTest() throws IOException, ClientSideException {
 
-        File file = new File(BOB_MACAROON);
-        StaticFileMacaroonContext context = new StaticFileMacaroonContext(file);
-        String currentMacaroonAsHex = context.getCurrentMacaroonAsHex();
+        // toBase64String already contains calling the encode function
+        byte[] macaroonBytes = Files.readAllBytes(Paths.get(BOB_MACAROON));
+        String macaroonBase64 = Base64.toBase64String(macaroonBytes);
 
+        String real_base64 = "AgELYy1saWdodG5pbmcCPk1vbiBNYXkgMjMgMjAyMiAwMzo1MzoyMSBHTVQrMDAwMCAoQ29vcmRpbmF0ZWQgVW5pdmVyc2FsIFRpbWUpAAAGIFt67R66vxKgyh+4mRWNozBf+La8r4cxrTQWgeH6Laxp";
+        if (macaroonBase64.equals(real_base64)) {
+            System.out.println("Base64 equals");
+        }
 
-//        Request requestGet = new Request.Builder()
-//                .url(BOB_REST_API + "/v1/getinfo")
-//                .header("macaroon", currentMacaroonAsHex)
-//                .build();
-//
-//        Call call = okHttpClient.newCall(requestGet);
-//        RequestBody body = call.request().body();
-//        System.out.println();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(BOB_REST_API + "v1/getinfo").newBuilder();
+        String url = urlBuilder
+                .build()
+                .toString();
 
+        Request requestGet = new Request.Builder()
+                .url(url)
+                .header("macaroon", macaroonBase64)
+                .build();
+
+        Call call = okHttpClient.newCall(requestGet);
+
+        ResponseBody body = call.execute().body();
+        if (null != body) {
+            System.out.println();
+            System.out.println(body.string());
+            System.out.println();
+        }
     }
-
-
 }
