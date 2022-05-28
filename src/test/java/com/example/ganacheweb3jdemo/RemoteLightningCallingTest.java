dc7bec0c168e5a1f944b4d7dc5f96003cc045f5f
lykismekk <lykismekk@outlook.com>
2022-05-28 15:49:19 +0800

    1. adding key manager for ssl request 2. delete the protobuf stuff for helping run the application on wins

--- src/test/java/com/example/ganacheweb3jdemo/RemoteLightningCallingTest.java
+++ src/test/java/com/example/ganacheweb3jdemo/RemoteLightningCallingTest.java
@@ -5,11 +5,10 @@ import org.apache.commons.codec.binary.Hex;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.*;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
@@ -30,7 +29,12 @@ public class RemoteLightningCallingTest {
     private final static String NODE_IP = "18.139.32.218";
     private final static int NODE_GRPC_PORT = 10009;
     private final static int NODE_REST_PORT = 9735;
-    private final static String FILE_BASE_PATH = "/Users/pundix2022/Desktop/开发项目/LightningNetworkRelated/";
+
+//    private final static String FILE_BASE_PATH = "/Users/pundix2022/Desktop/开发项目/LightningNetworkRelated/";
+//    private final static String CERT_PATH = FILE_BASE_PATH + "tls.cert";
+//    private final static String MACAROON_PATH = FILE_BASE_PATH + "admin.macaroon";
+
+    private final static String FILE_BASE_PATH = "D:\\lightning\\";
     private final static String CERT_PATH = FILE_BASE_PATH + "tls.cert";
     private final static String MACAROON_PATH = FILE_BASE_PATH + "admin.macaroon";
 
@@ -50,13 +54,18 @@ public class RemoteLightningCallingTest {
                 .header("Grpc-Metadata-macaroon", macaroonHexStr)
                 .build();
 
-        X509TrustManager TRUST_FILES_CERTS = generateTrustManagerByFile(certificate);
+        // create suitable trust manager & key manager for https request
         SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, new TrustManager[]{TRUST_FILES_CERTS}, new java.security.SecureRandom());
+        X509TrustManager x509TrustManager = generateTrustManagerByFile(certificate);
+        X509KeyManager x509KeyManager = generateKeyManagerByFile(certificate);
+        sslContext.init(
+                new KeyManager[]{x509KeyManager},
+                new TrustManager[]{x509TrustManager},
+                new java.security.SecureRandom());
 
 
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
-                .sslSocketFactory(sslContext.getSocketFactory(), TRUST_FILES_CERTS)
+                .sslSocketFactory(sslContext.getSocketFactory(), x509TrustManager)
                 .build();
         Call call = okHttpClient.newCall(requestGet);
 
@@ -67,6 +76,46 @@ public class RemoteLightningCallingTest {
             System.out.println();
         }
 
+    }
+
+    @NotNull
+    private X509KeyManager generateKeyManagerByFile(Certificate certificate) throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException {
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(null, null);
+
+        X509KeyManager manager = (X509KeyManager) kmf.getKeyManagers()[0];
+        return new X509KeyManager() {
+            @Override
+            public String[] getClientAliases(String keyType, Principal[] issuers) {
+                return manager.getServerAliases(keyType, issuers);
+            }
+
+            @Override
+            public String chooseClientAlias(String[] keyType, Principal[] issuers, Socket socket) {
+                return manager.chooseClientAlias(keyType, issuers, socket);
+            }
+
+            @Override
+            public String[] getServerAliases(String keyType, Principal[] issuers) {
+                return manager.getServerAliases(keyType, issuers);
+            }
+
+            @Override
+            public String chooseServerAlias(String keyType, Principal[] issuers, Socket socket) {
+                return manager.chooseServerAlias(keyType, issuers, socket);
+            }
+
+            @Override
+            public X509Certificate[] getCertificateChain(String alias) {
+                // Here to get the appropriate file
+                return new X509Certificate[] {(X509Certificate) certificate};
+            }
+
+            @Override
+            public PrivateKey getPrivateKey(String alias) {
+                return null;
+            }
+        };
     }
 
     @NotNull
