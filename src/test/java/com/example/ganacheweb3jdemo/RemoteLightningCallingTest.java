e5deeeff11651b993883fc6bb2039a68a64c18b4
lykismekk <lykismekk@outlook.com>
2022-05-28 20:57:54 +0800

    adding thorough comment for every steps on build ssl socket by okhttp

--- src/test/java/com/example/ganacheweb3jdemo/RemoteLightningCallingTest.java
+++ src/test/java/com/example/ganacheweb3jdemo/RemoteLightningCallingTest.java
@@ -20,6 +20,9 @@ import java.security.cert.X509Certificate;
 
 /**
  * 尝试进行RPC解析测试 - Remote
+ * cert: https://www.baeldung.com/okhttp-self-signed-cert
+ * trust: https://github.com/lightningnetwork/lnd/blob/master/docs/grpc/java.md
+ * hostname: https://stackoverflow.com/questions/31917988/okhttp-javax-net-ssl-sslpeerunverifiedexception-hostname-domain-com-not-verifie
  *
  * @author Roylic
  * @date 2022/4/21
@@ -43,30 +46,37 @@ public class RemoteLightningCallingTest {
     @Test
     public void connectionTest() throws IOException, CertificateException, NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
 
+        // get certificate
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        // generate certificate instance from input file stream, this certificate would be used in verifying response
         final Certificate certificate = cf.generateCertificate(new FileInputStream(CERT_PATH));
 
+        // lnd only receive hex string without '0x' as header, thus, if we using Numeric.toHexString, must cut '0x'
         String macaroonHexStr = Hex.encodeHexString(Files.readAllBytes(Paths.get(MACAROON_PATH)));
 
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://" + NODE_IP + ":" + NODE_REST_PORT + "/v1/getinfo").newBuilder();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://" + NODE_IP + ":" + NODE_REST_PORT + "/v1/channels").newBuilder();
         Request requestGet = new Request.Builder()
                 .url(urlBuilder.build().toString())
+                // lnd node receive Macaroon Hex as Header below
                 .header("Grpc-Metadata-macaroon", macaroonHexStr)
                 .build();
 
-        // create suitable trust manager & key manager for https request
+        // ssl context would be specific using TLS
         SSLContext sslContext = SSLContext.getInstance("TLS");
+        // create suitable trust manager & key manager for https request
         X509TrustManager x509TrustManager = generateTrustManagerByFile(certificate);
         X509KeyManager x509KeyManager = generateKeyManagerByFile(certificate);
-        HostnameVerifier hostnameVerifier = generateHostnameVerifier();
         sslContext.init(
                 new KeyManager[]{x509KeyManager},
                 new TrustManager[]{x509TrustManager},
                 new java.security.SecureRandom());
 
-
+        // if we requesting by ip directly, must create suitable hostname verifier
+        HostnameVerifier hostnameVerifier = generateHostnameVerifier();
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                // set socket factory
                 .sslSocketFactory(sslContext.getSocketFactory(), x509TrustManager)
+                // set hostname verifier
                 .hostnameVerifier(hostnameVerifier)
                 .build();
         Call call = okHttpClient.newCall(requestGet);
@@ -80,13 +90,22 @@ public class RemoteLightningCallingTest {
 
     }
 
+    /**
+     * Key Manager specific on requesting to the remote server
+     *
+     * @param certificate tls.cert
+     * @return X509KeyManager
+     */
     @NotNull
     private X509KeyManager generateKeyManagerByFile(Certificate certificate)
             throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException {
 
+        // get the key manager from jvm
         KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        // must initial it with this line
         kmf.init(null, null);
 
+        // get the first manager and add it's related stuff into the one we wanna generate
         X509KeyManager manager = (X509KeyManager) kmf.getKeyManagers()[0];
         return new X509KeyManager() {
             @Override
@@ -109,38 +128,59 @@ public class RemoteLightningCallingTest {
                 return manager.chooseServerAlias(keyType, issuers, socket);
             }
 
+            /**
+             * For this specific request, we know out destination,
+             * thus, for all socket from this okhttp instance,
+             * we can just return this only certificate
+             */
             @Override
             public X509Certificate[] getCertificateChain(String alias) {
                 // Here to get the appropriate file
-                return new X509Certificate[] {(X509Certificate) certificate};
+                return new X509Certificate[]{(X509Certificate) certificate};
             }
 
             @Override
             public PrivateKey getPrivateKey(String alias) {
+                // not realy exchange the key to the server
                 return null;
             }
         };
     }
 
+
+    /**
+     * Trust Manager specific on the responding from the server
+     *
+     * @param certificate tls.cert
+     * @return X509TrustManager
+     */
     @NotNull
     private X509TrustManager generateTrustManagerByFile(Certificate certificate) {
         return new X509TrustManager() {
+            /**
+             * verify if client connect to us
+             */
             @Override
             public void checkClientTrusted(X509Certificate[] chain, String authType) {
                 try {
                     certificate.verify(chain[0].getPublicKey());
-                } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException | NoSuchProviderException | SignatureException e) {
+                } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException
+                        | NoSuchProviderException | SignatureException e) {
                     System.out.println("Client Illegal");
                     e.printStackTrace();
                 }
                 System.out.println("Client Trusted");
             }
 
+            /**
+             * verify server's response
+             */
             @Override
             public void checkServerTrusted(X509Certificate[] chain, String authType) {
                 try {
                     certificate.verify(chain[0].getPublicKey());
-                } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException | NoSuchProviderException | SignatureException e) {
+                } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException
+                        | NoSuchProviderException | SignatureException e) {
                     System.out.println("Server Illegal");
                     e.printStackTrace();
                 }
@@ -154,8 +194,16 @@ public class RemoteLightningCallingTest {
         };
     }
 
+    /**
+     * new host name verifier that accept all host
+     *
+     * @return HostnameVerifier
+     */
     @NotNull
     private HostnameVerifier generateHostnameVerifier() {
+        // because we are using the ip to call the server,
+        // for host name verification, we just true for all
+        // or else get block...
         return (hostname, session) -> true;
     }
 }
