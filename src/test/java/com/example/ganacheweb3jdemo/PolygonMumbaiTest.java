cd05b4b15e628e55334196354ca4197a2b2185de
Roylic <roylic@wokoworks.com>
2022-06-01 17:50:09 +0800

    successfully doing multiple operations interact with Mumbai Testnet

--- src/test/java/com/example/ganacheweb3jdemo/PolygonMumbaiTest.java
+++ src/test/java/com/example/ganacheweb3jdemo/PolygonMumbaiTest.java
@@ -9,22 +9,29 @@ import java.util.concurrent.TimeUnit;
 /**
  * Doc:
  * https://docs.polygon.technology/docs/develop/network-details/network/
- *
+ * <p>
  * Open-Api:
- * https://open-api-staging.polygon.technology/api-docs/#/Balance
- *
+ * https://edge-docs.polygon.technology/docs/get-started/json-rpc-commands/#eth_chainid
+ * <p>
  * Testnet-Rpc:
+ * https://rpc.poa.psdk.io:8545
  * https://rpc-mumbai.matic.today
  * https://matic-mumbai.chainstacklabs.com
  * https://rpc-mumbai.maticvigil.com
  * https://matic-testnet-archive-rpc.bwarelabs.com
+ *
+ * Mainnet-Api:
+ * https://polygon.io/docs/stocks/getting-started
  *
  * @author Roylic
  * 2022/5/12
  */
 public class PolygonMumbaiTest {
 
-    private final static String POLYGON_MUMBAI_TESTNET_PATH = "https://rpc.poa.psdk.io:8545";
+    private final static String POLYGON_MUMBAI_TESTNET_PATH = "https://rpc-mumbai.matic.today";
+
+    private final static String WALLET_PATH = "0x70076F9f8e221d4729314f99a8AB410C117560aB";
+
 
     private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .readTimeout(10, TimeUnit.SECONDS)
@@ -35,10 +42,19 @@ public class PolygonMumbaiTest {
     @Test
     public void simpleConnection() throws IOException {
 
-        // Polygon Api 路径不变, 主要在于请求内容不同;
+        // Polygon Api 路径不变, 主要在于请求内容不同
+        String connectData = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_chainId\",\"params\":[],\"id\":1}";
+        String blockNumData = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[],\"id\":1}";
+        String balanceData = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"" + WALLET_PATH + "\", \"latest\"],\"id\":1}";
+        String txnCountData = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getTransactionCount\",\"params\":[\"" + WALLET_PATH + "\",\"latest\"],\"id\":1}";
+
+        requestAndPrintf(txnCountData);
+    }
+
+    private void requestAndPrintf(String reqData) throws IOException {
         Request requestPost = new Request.Builder()
                 .url(POLYGON_MUMBAI_TESTNET_PATH)
-                .post(RequestBody.create("{\"jsonrpc\":\"2.0\",\"method\":\"eth_chainId\",\"params\":[],\"id\":1}", MediaType.parse("application/json")))
+                .post(RequestBody.create(reqData, MediaType.parse("application/json")))
                 .build();
 
         Call call = okHttpClient.newCall(requestPost);
@@ -46,7 +62,6 @@ public class PolygonMumbaiTest {
         if (null != body) {
             System.out.println(body.string());
         }
-
     }
 
 }
