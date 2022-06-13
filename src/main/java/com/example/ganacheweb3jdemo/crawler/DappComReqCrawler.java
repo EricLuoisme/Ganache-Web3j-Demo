bd93d593227d1b6fc643d2976ff2d7e0020d02f4
Roylic <roylic@wokoworks.com>
2022-06-13 18:53:52 +0800

    successfully exporting the dapp.com data to excel

--- src/main/java/com/example/ganacheweb3jdemo/crawler/DappComReqCrawler.java
+++ src/main/java/com/example/ganacheweb3jdemo/crawler/DappComReqCrawler.java
@@ -1,15 +1,14 @@
 package com.example.ganacheweb3jdemo.crawler;
 
+import com.alibaba.excel.EasyExcel;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import okhttp3.*;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author Roylic
@@ -25,8 +24,8 @@ public class DappComReqCrawler {
 
         List<DappComDetail> resultList = new LinkedList<>();
 
-        // try 50 pages first
-        for (int i = 0; i < 2; i++) {
+        // try 25 pages first
+        for (int i = 0; i < 25; i++) {
             // building url
             HttpUrl.Builder urlBuilder = HttpUrl.parse("https://www.dapp.com/api/ranking/dapp/?chain=0&page=" + (i + 1) + "&sort=usd_24h").newBuilder();
             String url = urlBuilder
@@ -56,13 +55,14 @@ public class DappComReqCrawler {
                     JSONObject categoryObj = dapp.getJSONObject("category");
                     dappComDetail.setCategory((String) categoryObj.get("name"));
 
-                    Set<String> chains = new HashSet<>();
+                    StringBuilder chains = new StringBuilder();
                     JSONArray chainsArr = dapp.getJSONArray("chains");
                     chainsArr.forEach(chain -> {
                         JSONObject chainObj = (JSONObject) chain;
-                        chains.add((String) chainObj.get("name"));
+                        chains.append(chainObj.get("name"))
+                                .append(",");
                     });
-                    dappComDetail.setChains(chains);
+                    dappComDetail.setChains(chains.toString());
 
                     dappComDetail.setSocial_signal((Integer) dapp.get("social_signal"));
                     dappComDetail.setSocial_signal_gr((BigDecimal) dapp.get("social_signal_gr"));
@@ -73,8 +73,20 @@ public class DappComReqCrawler {
             System.out.println(">>> finished page:" + (i + 1));
         }
 
-        System.out.println();
+        outputAsFile(resultList);
 
+        System.out.println("<<< all finished");
+    }
+
+    private static void outputAsFile(List<DappComDetail> dappComDetails) throws IOException {
+        System.out.println(">>> start formatting file");
+
+        String fileName = "dapp_com_first_25_page.xlsx";
+        EasyExcel.write(fileName, DappComDetail.class)
+                        .sheet("dappCom")
+                                .doWrite(dappComDetails);
+
+        System.out.println("<<< finished formatting file");
     }
 
 
