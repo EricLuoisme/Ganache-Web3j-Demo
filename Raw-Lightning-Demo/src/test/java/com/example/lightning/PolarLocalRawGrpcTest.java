954ac34cb4e2228f24bee1e097b78ae93cd1f8e5
lykismekk <lykismekk@outlook.com>
2022-06-25 14:54:04 +0800

    formatted the credential methods to connect to Polar's Lnd with raw GRPC

--- Raw-Lightning-Demo/src/test/java/com/example/lightning/PolarLocalRawGrpcTest.java
+++ Raw-Lightning-Demo/src/test/java/com/example/lightning/PolarLocalRawGrpcTest.java
@@ -1,7 +1,9 @@
 package com.example.lightning;
 
 import com.example.lighting.MacaroonCallCredential;
+import com.example.lightning.LightningGrpc.LightningBlockingStub;
 import com.example.lightning.router.RouterGrpc;
+import com.example.lightning.router.RouterGrpc.RouterBlockingStub;
 import com.example.lightning.router.TrackPaymentRequest;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
@@ -29,48 +31,48 @@ public class PolarLocalRawGrpcTest {
 
     private final static String POLAR_BASE_URL = "https://127.0.0.1:";
     private final static String POLAR_MACAROON_LOC = "/Users/pundix2022/.polar/networks/1/volumes/lnd";
+    private final static String POLAR_FILE_LOC_WIN = "C:\\Users\\lykis\\.polar\\networks\\1\\volumes\\lnd";
 
     // Alice
     private final static int ALICE_GRPC_PORT = 10001;
     private final static String ALICE_REST_HOST = "https://127.0.0.1:8081";
-    private final static String ALICE_CERT = POLAR_MACAROON_LOC + "/alice/tls.cert";
-    private final static String ALICE_MACAROON = POLAR_MACAROON_LOC + "/alice/data/chain/bitcoin/regtest/admin.macaroon";
-    private final static String ALICE_INVOICE_MACAROON = POLAR_MACAROON_LOC + "/alice/data/chain/bitcoin/regtest/invoice.macaroon";
+    private final static String ALICE_CERT = "/alice/tls.cert";
+    private final static String ALICE_MACAROON = "/alice/data/chain/bitcoin/regtest/admin.macaroon";
     private final static String ALICE_PUB_KEY = "02c94f36b8574122ecf189a90fea84f02b8b80c21577f60499a2345ae9621c9fb4";
 
     // Dave
     private final static int DAVE_GRPC_PORT = 10004;
     private final static String DAVE_REST_HOST = "https://127.0.0.1:8084";
-    private final static String DAVE_CERT = POLAR_MACAROON_LOC + "/dave/tls.cert";
-    private final static String DAVE_MACAROON = POLAR_MACAROON_LOC + "/dave/data/chain/bitcoin/regtest/admin.macaroon";
+    private final static String DAVE_CERT = "/dave/tls.cert";
+    private final static String DAVE_MACAROON = "/dave/data/chain/bitcoin/regtest/admin.macaroon";
     private final static String DAVE_PUB_KEY = "03b042a9d98111696f7aa00815dd8c740a55afb452bf89de33a3001ce8a03f8f65";
 
     // Erin
     private final static int ERIN_GRPC_PORT = 10005;
     private final static String ERIN_REST_HOST = "https://127.0.0.1:8085";
-    private final static String ERIN_CERT = POLAR_MACAROON_LOC + "/erin/tls.cert";
-    private final static String ERIN_MACAROON = POLAR_MACAROON_LOC + "/erin/data/chain/bitcoin/regtest/admin.macaroon";
+    private final static String ERIN_CERT = "/erin/tls.cert";
+    private final static String ERIN_MACAROON = "/erin/data/chain/bitcoin/regtest/admin.macaroon";
     private final static String ERIN_PUB_KEY = "034236bdc8b2b4d5ad0c3292364d514f5fc12008172531d6fe48853553a9948a7b";
 
 
     @Test
+    public void connectionTest() throws IOException {
+        LightningBlockingStub lightningBlockingStub = getLightningBlockingStub(
+                POLAR_FILE_LOC_WIN, ALICE_CERT, ALICE_GRPC_PORT, ALICE_MACAROON);
+        WalletBalanceResponse walletBalanceResponse = lightningBlockingStub.walletBalance(WalletBalanceRequest.newBuilder().build());
+        System.out.println(walletBalanceResponse.toString());
+    }
+
+
+    @Test
     public void decodePaymentReqTest() throws IOException {
-        ManagedChannel channel = getChannel("127.0.0.1", ALICE_CERT, ALICE_GRPC_PORT);
-        String macaroon =
-                Hex.encodeHexString(
-                        Files.readAllBytes(Paths.get(ALICE_MACAROON))
-                );
-
         // stub
-        LightningGrpc.LightningBlockingStub lightningBlockingStub = LightningGrpc
-                .newBlockingStub(channel)
-                .withCallCredentials(new MacaroonCallCredential(macaroon));
-
+        LightningBlockingStub lightningBlockingStub = getLightningBlockingStub(
+                POLAR_FILE_LOC_WIN, DAVE_CERT, DAVE_GRPC_PORT, DAVE_MACAROON);
         // req
         PayReqString req = PayReqString.newBuilder()
                 .setPayReq("lnbcrt500u1p3ttpt4pp5l79yvv3narfv6gla9gvgfdttvf03j6s0fmqxvrnlpm6k3x76ne6sdqqcqzpgsp5y3ekzn4l9gwdupn26lusezr4eun5rkuxrluzv3ucumuq9y29xjzq9qyyssq00ytzu49tp7m3acgrq6kfx29gwncrxzmazcu0seczyg9jkvums8hdkx082klj39rej0z4mamvddzttl7vlhrf9w95tu6w26rq5sxe7cp3ukpxq")
                 .build();
-
         PayReq payReq = lightningBlockingStub.decodePayReq(req);
         System.out.println(payReq.toString());
         System.out.println(">>> Hex Payment Addr" + Hex.encodeHexString(payReq.getPaymentAddr().toByteArray()));
@@ -79,16 +81,8 @@ public class PolarLocalRawGrpcTest {
 
     @Test
     public void trackPaymentTest() throws IOException {
-        ManagedChannel channel = getChannel("127.0.0.1", ALICE_CERT, ALICE_GRPC_PORT);
-        String macaroon =
-                Hex.encodeHexString(
-                        Files.readAllBytes(Paths.get(ALICE_MACAROON))
-                );
-
-        // stub
-        RouterGrpc.RouterBlockingStub routerBlockingStub = RouterGrpc.
-                newBlockingStub(channel)
-                .withCallCredentials(new MacaroonCallCredential(macaroon));
+        RouterBlockingStub routerBlockingStub = getRouterBlockingStub(
+                POLAR_FILE_LOC_WIN, ALICE_CERT, ALICE_GRPC_PORT, ALICE_MACAROON);
 
         // req
         TrackPaymentRequest req = TrackPaymentRequest.newBuilder()
@@ -101,6 +95,25 @@ public class PolarLocalRawGrpcTest {
         while (paymentIterator.hasNext()) {
             System.out.println(paymentIterator.next().toString());
         }
+    }
+
+
+    private static LightningBlockingStub getLightningBlockingStub(String filePath, String certPath, Integer grpcPort, String macaroonPath) throws IOException {
+        ManagedChannel channel = getChannel("127.0.0.1", filePath + certPath, grpcPort);
+        String macaroon = Hex.encodeHexString(Files.readAllBytes(Paths.get(filePath + macaroonPath)));
+        // stub
+        return LightningGrpc
+                .newBlockingStub(channel)
+                .withCallCredentials(new MacaroonCallCredential(macaroon));
+    }
+
+    private static RouterBlockingStub getRouterBlockingStub(String filePath, String certPath, Integer grpcPort, String macaroonPath) throws IOException {
+        ManagedChannel channel = getChannel("127.0.0.1", filePath + certPath, grpcPort);
+        String macaroon = Hex.encodeHexString(Files.readAllBytes(Paths.get(filePath + macaroonPath)));
+        // stub
+        return RouterGrpc
+                .newBlockingStub(channel)
+                .withCallCredentials(new MacaroonCallCredential(macaroon));
     }
 
     // form ssl channel
