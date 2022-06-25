272cd114d06d18c23eb911115bf7ad69a3977900
lykismekk <lykismekk@outlook.com>
2022-06-26 00:34:27 +0800

    adding stuffs

--- Raw-Lightning-Demo/src/test/java/com/example/lightning/PolarLocalRawGrpcTest.java
+++ Raw-Lightning-Demo/src/test/java/com/example/lightning/PolarLocalRawGrpcTest.java
@@ -2,11 +2,8 @@ package com.example.lightning;
 
 import com.example.lighting.MacaroonCallCredential;
 import com.example.lightning.LightningGrpc.LightningBlockingStub;
-import com.example.lightning.router.RouteFeeRequest;
-import com.example.lightning.router.RouteFeeResponse;
-import com.example.lightning.router.RouterGrpc;
+import com.example.lightning.router.*;
 import com.example.lightning.router.RouterGrpc.RouterBlockingStub;
-import com.example.lightning.router.TrackPaymentRequest;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.StatusRuntimeException;
@@ -74,13 +71,19 @@ public class PolarLocalRawGrpcTest {
     }
 
 
+    /**
+     * Decode for getting:
+     * - num_msat -> check mis-behavior
+     * - payment hash -> tack payment use
+     * - destination + timestamp + expiry -> send payment use
+     */
     @Test
     public void decodePaymentReqTest() throws IOException {
         // stub
         LightningBlockingStub lightningBlockingStub = getLightningBlockingStub(
                 POLAR_FILE_LOC_WIN, DAVE_CERT, DAVE_GRPC_PORT, DAVE_MACAROON);
 
-        String payReqStr = "lnbcrt100u1p3tdvf0pp53qq0usgdmxvaykgcwrtjcka0lalzt85yglq3hkfsty7ycwvxpcmqdqqcqzpgsp5tule4tyrhj2a50mxhxh4gw9px0hlyktvtjgw6r4nnkw2j5x6gvqq9qyyssqq5vegu6w48p9ac0clyc9lxuu7nvywgcfmth9ccq223j2ysafnfdyka60qa62xqppdsense2e9mk5lvuz4n325wzth0qec8s4f82rlkcqycj39p";
+        String payReqStr = "lnbcrt100u1p3twdanpp5tmu6j2d9gwxuh4x5n7wktgpk8ueehjsflx50nxgzm3e40yw993vsdqqcqzpgsp5hrt93chtnummpdcsxgjmkp9vt5nux6gr4g39pkf5rr95lwmvmwps9qyyssqnypjks7p2fnt5dzcq25a9hjtgn3de8ffc5gskmvh6du90e5799l389sgmsg2cg0583lrgc03vnfs4n3z7dtjhu3hhxrdkqpsjssya4qqsd07pu";
 
         // req
         PayReqString req = PayReqString.newBuilder()
@@ -88,9 +91,14 @@ public class PolarLocalRawGrpcTest {
                 .build();
         PayReq payReq = lightningBlockingStub.decodePayReq(req);
         System.out.println(payReq.toString());
-        System.out.println(">>> Hex Payment Addr" + Hex.encodeHexString(payReq.getPaymentAddr().toByteArray()));
+        System.out.println(">>> Payment Hash: " + payReq.getPaymentHash());
+//        System.out.println(">>> Hex Payment Addr: " + Hex.encodeHexString(payReq.getPaymentAddr().toByteArray()));
     }
 
+    /**
+     * Estimate the route fee for getting:
+     * - routingFeeMSat -> send payment use
+     */
     @Test
     public void estimateRouteFee() throws IOException {
         // stub
@@ -111,24 +119,57 @@ public class PolarLocalRawGrpcTest {
                 POLAR_FILE_LOC_WIN, ALICE_CERT, ALICE_GRPC_PORT, ALICE_MACAROON);
         QueryRoutesRequest routeReq = QueryRoutesRequest.newBuilder()
                 .setAmt(10000)
+                .setFeeLimit(
+                        FeeLimit.newBuilder()
+//                                .setFixedMsat(1000)
+                                .build())
                 .setPubKey(ERIN_PUB_KEY)
                 .build();
         QueryRoutesResponse queryRoutesResponse = lightningBlockingStub.queryRoutes(routeReq);
         System.out.println(">>> " + queryRoutesResponse);
+        System.out.println(">>> Hops: " + queryRoutesResponse.getRoutesList().size());
+    }
+
+    /**
+     * Send payment synchronised, with inputs:
+     * -
+     */
+    @Test
+    public void sendPaymentTest() throws IOException {
+        LightningBlockingStub lightningBlockingStub = getLightningBlockingStub(
+                POLAR_FILE_LOC_WIN, ALICE_CERT, ALICE_GRPC_PORT, ALICE_MACAROON);
+
+        String payReqStr = "lnbcrt100u1p3twdanpp5tmu6j2d9gwxuh4x5n7wktgpk8ueehjsflx50nxgzm3e40yw993vsdqqcqzpgsp5hrt93chtnummpdcsxgjmkp9vt5nux6gr4g39pkf5rr95lwmvmwps9qyyssqnypjks7p2fnt5dzcq25a9hjtgn3de8ffc5gskmvh6du90e5799l389sgmsg2cg0583lrgc03vnfs4n3z7dtjhu3hhxrdkqpsjssya4qqsd07pu";
+        SendRequest req = SendRequest.newBuilder()
+                .setPaymentRequest(payReqStr)
+                .setFeeLimit(
+                        FeeLimit.newBuilder()
+                                .setFixedMsat(1010)
+                                .build())
+                .build();
+
+        SendResponse sendResponse = lightningBlockingStub.sendPaymentSync(req);
+        System.out.println(sendResponse.toString());
     }
 
 
-
+    /**
+     * Judge an payment is been paid, using the PaymentHash from decode-req-result
+     * Only the one who paid the invoice could got an outcome, otherwise, exception
+     */
     @Test
     public void trackPaymentTest() throws IOException {
         RouterBlockingStub routerBlockingStub = getRouterBlockingStub(
                 POLAR_FILE_LOC_WIN, ERIN_CERT, ERIN_GRPC_PORT, ERIN_MACAROON);
 
+//        RouterBlockingStub routerBlockingStub = getRouterBlockingStub(
+//                POLAR_FILE_LOC_WIN, ALICE_CERT, ALICE_GRPC_PORT, ALICE_MACAROON);
+
         // req
         TrackPaymentRequest req = TrackPaymentRequest.newBuilder()
                 .setPaymentHash(
                         ByteString.copyFrom(
-                                Numeric.hexStringToByteArray("8800fe410dd999d2591870d72c5bafff7e259e8447c11bd930593c4c39860e36")))
+                                Numeric.hexStringToByteArray("5ef9a929a5438dcbd4d49f9d65a0363f339bca09f9a8f99902dc735791c52c59")))
                 .build();
 
         try {
@@ -149,6 +190,21 @@ public class PolarLocalRawGrpcTest {
 
     }
 
+
+    @Test
+    public void subscribeInvoice() throws IOException {
+        LightningBlockingStub lightningBlockingStub = getLightningBlockingStub(
+                POLAR_FILE_LOC_WIN, ERIN_CERT, ERIN_GRPC_PORT, ERIN_MACAROON);
+
+        Iterator<Invoice> invoiceIterator = lightningBlockingStub.subscribeInvoices(InvoiceSubscription.newBuilder().build());
+        while (invoiceIterator.hasNext()) {
+            Invoice next = invoiceIterator.next();
+            System.out.println("This Invoice Status: " + next.getState().name());
+            if (next.getState().getNumber() == 1) {
+                System.out.println("Detail for this Settled Invoice: " + next.toString());
+            }
+        }
+    }
 
     private static LightningBlockingStub getLightningBlockingStub(String filePath, String certPath, Integer grpcPort, String macaroonPath) throws IOException {
         ManagedChannel channel = getChannel("127.0.0.1", filePath + certPath, grpcPort);
