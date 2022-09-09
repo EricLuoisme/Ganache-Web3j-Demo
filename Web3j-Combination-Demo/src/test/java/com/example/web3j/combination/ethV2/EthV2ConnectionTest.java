559805c5fe7ea563a44a9c8659d7fb3d93b84e9b
roylic <lykismekk@outlook.com>
2022-09-09 16:18:09 +0800

    adding Goerli Testnet connection test

---
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/ethV2/EthV2ConnectionTest.java
@@ -0,0 +1,39 @@
+package com.example.web3j.combination.ethV2;
+
+import org.junit.jupiter.api.Test;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
+
+public class EthV2ConnectionTest {
+
+
+    public static final Web3j web3j = Web3j.build(
+            new HttpService("https://goerli.infura.io/v3/3f0482cf4c3545dbabaeab75f414e467"));
+
+    @Test
+    public void testConnection() {
+        Web3ClientVersion web3ClientVersion = null;
+        try {
+            web3ClientVersion = web3j.web3ClientVersion().send();
+            String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+            System.out.println();
+            System.out.println(clientVersion);
+            System.out.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void getLatestBlock() throws IOException {
+        EthBlock.Block block = web3j.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false)
+                .send().getBlock();
+        System.out.println(block.getNumber().toString());
+    }
+
+}
