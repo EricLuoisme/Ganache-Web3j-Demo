663a412b33ff7bff76f483180c480fefb740b25a
Roylic <roylic@wokoworks.com>
2022-10-28 10:18:50 +0800

    editing for auto changing date

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/GoerliGasFeeTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/GoerliGasFeeTest.java
@@ -4,6 +4,8 @@ import okhttp3.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 查询GasFee
@@ -15,11 +17,18 @@ public class GoerliGasFeeTest {
 
     private static final OkHttpClient client = new OkHttpClient().newBuilder().build();
     private static final MediaType mediaType = MediaType.parse("application/json");
+    private static final String REQUEST_TEMPLATE = "{\"query\":\"\\n            query ($network: EthereumNetwork!,\\n                  $dateFormat: String!,\\n\\n                  $from: ISO8601DateTime,\\n                  $till: ISO8601DateTime){\\n                    ethereum(network: $network ){\\n                      transactions(options:{asc: \\\"date.date\\\"}, date: {\\n                        since: $from\\n                        till: $till}\\n\\n                      ) {\\n                        date: date{\\n                          date(format: $dateFormat)\\n                        }\\n                        gasPrice\\n                        gasValue\\n                        average: gasValue(calculate: average )\\n                        maxGasPrice: gasPrice(calculate: maximum)\\n                        medianGasPrice: gasPrice(calculate: median)\\n                      }\\n                    }\\n                  }\",\"variables\":\"{\\\"limit\\\":10,\\\"offset\\\":0,\\\"network\\\":\\\"goerli\\\",\\\"from\\\":\\\"%s\\\",\\\"till\\\":\\\"%sT23:59:59\\\",\\\"dateFormat\\\":\\\"%%Y-%%m-%%d\\\"}\"}";
 
 
     @Test
     public void querying() throws IOException {
-        RequestBody body = RequestBody.create("{\"query\":\"\\n            query ($network: EthereumNetwork!,\\n                  $dateFormat: String!,\\n\\n                  $from: ISO8601DateTime,\\n                  $till: ISO8601DateTime){\\n                    ethereum(network: $network ){\\n                      transactions(options:{asc: \\\"date.date\\\"}, date: {\\n                        since: $from\\n                        till: $till}\\n\\n                      ) {\\n                        date: date{\\n                          date(format: $dateFormat)\\n                        }\\n                        gasPrice\\n                        gasValue\\n                        average: gasValue(calculate: average )\\n                        maxGasPrice: gasPrice(calculate: maximum)\\n                        medianGasPrice: gasPrice(calculate: median)\\n                      }\\n                    }\\n                  }\",\"variables\":\"{\\\"limit\\\":10,\\\"offset\\\":0,\\\"network\\\":\\\"goerli\\\",\\\"from\\\":\\\"2022-10-27\\\",\\\"till\\\":\\\"2022-10-27T23:59:59\\\",\\\"dateFormat\\\":\\\"%Y-%m-%d\\\"}\"}", mediaType);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime now = LocalDateTime.now();
+        String usingDate = dtf.format(now);
+        String requestStr = String.format(REQUEST_TEMPLATE, usingDate, usingDate);
+
+        RequestBody body = RequestBody.create(requestStr, mediaType);
         Request request = new Request.Builder()
                 .url("https://graphql.bitquery.io")
                 .method("POST", body)
