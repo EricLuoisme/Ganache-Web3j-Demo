75580edcc2e1867b25f1c5c96176751ebfad5a32
roylic <roylic@wokoworks.com>
2022-11-08 18:35:56 +0800

    adding enum of calling url

--- E2Pay-Api-Demo/src/main/java/com/e2pay/demo/E2PayUriEnums.java
+++ E2Pay-Api-Demo/src/main/java/com/e2pay/demo/E2PayUriEnums.java
@@ -7,12 +7,33 @@ package com.e2pay.demo;
 public enum E2PayUriEnums {
 
 
-    AUTHORIZATION("/rest/h2h/authorization/");
+    AUTH_CODE("/rest/h2h/authorization/", BodyMediaType.JSON),
+
+    OAUTH_TOKEN("/rest/oauth/token", BodyMediaType.FORM),
+
+    CUSTOMER_INFO("/b2b/customer/me/account", BodyMediaType.NONE),
+
+    CUSTOMER_LIMIT("/b2b/customer/balance/limit", BodyMediaType.PATH_BODY),
+
+    QRIS_INQUIRY("/b2b/customer/me/transaction/qris/inquiry", BodyMediaType.PATH_BODY),
+
+    QRIS_PAYMENT("/b2b/customer/me/transaction/qris", BodyMediaType.JSON),
+
+
+    ;
 
 
     public String uri;
 
-    E2PayUriEnums(String uri) {
+    public BodyMediaType mediaType;
+
+    E2PayUriEnums(String uri, BodyMediaType mediaType) {
         this.uri = uri;
+        this.mediaType = mediaType;
+    }
+
+
+    public enum BodyMediaType {
+        JSON, FORM, PATH_BODY, NONE
     }
 }
