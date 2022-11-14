eab64499c24eb97821fcdfa676808ccbd7f57394
roylic <lykismekk@outlook.com>
2022-11-14 23:24:15 +0800

    successfully sending ERC-20 tokens with Eip-1559 standard

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthTransferTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthTransferTest.java
@@ -1,6 +1,10 @@
 package com.example.web3j.combination.eth;
 
 import org.junit.jupiter.api.Test;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.*;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.Hash;
 import org.web3j.crypto.RawTransaction;
@@ -9,6 +13,8 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.*;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
 import org.web3j.tx.Transfer;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
@@ -17,6 +23,8 @@ import org.web3j.utils.Numeric;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -34,11 +42,16 @@ public class EthTransferTest {
 
 
     @Test
+    public void getChainId() throws IOException {
+        EthChainId send = web3j.ethChainId().send();
+        System.out.println(send.getChainId());
+    }
+
+    @Test
     public void checkBalance() throws IOException {
         EthGetBalance send = web3j.ethGetBalance(address, DefaultBlockParameterName.LATEST).send();
         System.out.println(send.getBalance());
     }
-
 
     @Test
     public void decodeRecentTxnForGasPriceLimit() throws IOException {
@@ -81,7 +94,7 @@ public class EthTransferTest {
     }
 
     @Test
-    public void startTransfer_Eip1559() throws Exception {
+    public void startTransfer_Eip1559_Sync() throws Exception {
         Scanner in = new Scanner(System.in);
         System.out.println("PriKey: ");
         String priKeyUsing = in.nextLine();
@@ -93,6 +106,38 @@ public class EthTransferTest {
                 BigInteger.valueOf(2_000_000_000L), BigInteger.valueOf(10_000_000_000L)).send();
 
         System.out.println("Transaction hash: " + transactionReceipt.getTransactionHash());
+    }
+
+    @Test
+    public void startTransfer_Erc20_Eip1559_Async() throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("PriKey: ");
+        String priKeyUsing = in.nextLine();
+
+        // data for interacting with contract's specific method
+        // for method 'transfer', only have two input params
+        Function transfer = new Function("transfer",
+                Arrays.asList(new Address("0x36F0A040C8e60974d1F34b316B3e956f509Db7e5"), new Uint256(10L)),
+                Collections.singletonList(TypeReference.create(Bool.class)));
+
+        // encode the data field
+        String data = FunctionEncoder.encode(transfer);
+
+        // another stuff need to be filled
+        long chainId = 5; // for Goerli
+        BigInteger maxPriorityFeePerGas = BigInteger.valueOf(5_000_000_000L);
+        BigInteger maxFeePerGas = BigInteger.valueOf(50_000_000_000L);
+        BigInteger gasLimit = BigInteger.valueOf(50_000L);
+        String contract = "0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc";
+        BigInteger value = BigInteger.valueOf(10L);
+
+        // async transfer
+        Credentials credentials = Credentials.create(priKeyUsing);
+        TransactionManager tm = new RawTransactionManager(web3j, credentials);
+        EthSendTransaction ethSendTransaction = tm.sendEIP1559Transaction(chainId, maxPriorityFeePerGas, maxFeePerGas, gasLimit, contract, data, value);
+        String result = ethSendTransaction.getResult();
+
+        System.out.println("Transaction hash: " + result);
     }
 
 }
