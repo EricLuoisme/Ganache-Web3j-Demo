21700ed68ec7764a41bde3e42c3f16d031c0ec34
roylic <lykismekk@outlook.com>
2022-11-27 22:33:34 +0800

    adding Goerli valid erc-721 token decode method

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/ethV2/GoerliContractCallingTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/ethV2/GoerliContractCallingTest.java
@@ -1,11 +1,9 @@
-package com.example.web3j.combination.eth;
+package com.example.web3j.combination.ethV2;
 
+import com.example.web3j.combination.web3j.EthLogConstants;
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
-import org.web3j.abi.EventEncoder;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.FunctionReturnDecoder;
-import org.web3j.abi.TypeReference;
+import org.web3j.abi.*;
 import org.web3j.abi.datatypes.*;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.protocol.Web3j;
@@ -18,10 +16,14 @@ import org.web3j.protocol.core.methods.response.EthLog;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Roylic
@@ -34,6 +36,83 @@ public class GoerliContractCallingTest {
 
     public static final Web3j web3j
             = Web3j.build(new HttpService(INFURA_GOERLI_NODE_HTTP_LINK));
+
+
+    @Test
+    public void decode721() throws IOException {
+
+        long blockHeight = 8030381L;
+
+        // filter for event
+        EthFilter filter = new EthFilter(
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(blockHeight)),
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(blockHeight)),
+                Collections.emptyList());
+
+        Event transferEvent = new Event("Transfer",
+                Arrays.asList(
+                        TypeReference.create(Address.class, true),
+                        TypeReference.create(Address.class, true),
+                        // depends
+                        TypeReference.create(Uint256.class))
+        );
+
+        // add into filter
+        String topicStr = EventEncoder.encode(transferEvent);
+        filter.addOptionalTopics(topicStr);
+
+        // decode
+        EthLog ethLogResp = web3j.ethGetLogs(filter).send();
+        ethLogResp.getLogs().forEach(ethLog -> {
+            EthLog.LogObject logObject = (EthLog.LogObject) ethLog;
+            // erc-721 data = 0x, erc-20 data = amount of token
+            if (logObject.getData().equals("0x")) {
+
+                Iterator<String> iterator = logObject.getTopics().iterator();
+
+                // 1. signature
+                iterator.next();
+
+                // 2. from address
+                String from = new Address(iterator.next()).getValue();
+
+                // 3. to address
+                String to = new Address(iterator.next()).getValue();
+
+                // 4. amount (erc-721 only supported transfer single token each time)
+                String valueStr = iterator.next();
+                List<Type> valueDecodeList = FunctionReturnDecoder.decode(valueStr, Utils.convert(EthLogConstants.EthFuncOutput.UINT256.getFuncOutputParams()));
+                BigDecimal amount = BigDecimal.ONE;
+
+                // 5. decode and get token id
+//                List<TypeReference<?>> unit256 = Stream.of(TypeReference.create(Uint256.class)).collect(Collectors.toList());
+                List<TypeReference<?>> unit256 = Collections.singletonList(TypeReference.create(Uint256.class));
+                List<Type> tokenIdDecode = FunctionReturnDecoder.decode(valueStr, Utils.convert(unit256));
+                Uint256 tokenId = (Uint256) tokenIdDecode.get(0);
+                System.out.println("Token id: " + tokenId.getValue().toString());
+
+                Function tokenUriFunc = new Function(
+                        "tokenURI",
+                        Collections.singletonList(tokenId),
+                        Collections.singletonList(TypeReference.create(Utf8String.class)));
+
+                String encode = FunctionEncoder.encode(tokenUriFunc);
+                System.out.println("ERC-721 tokenURI function's signature >>> " + encode.substring(0, 10));
+
+                Transaction reqTxn = Transaction.createEthCallTransaction(logObject.getAddress(), logObject.getAddress(), encode);
+                try {
+                    EthCall result = web3j.ethCall(reqTxn, DefaultBlockParameterName.LATEST).send();
+                    List<Type> decode = FunctionReturnDecoder.decode(result.getValue(), tokenUriFunc.getOutputParameters());
+                    Utf8String tokenUri = (Utf8String) decode.get(0);
+                    System.out.println("ERC-721 token uri >>> " + tokenUri.getValue());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
 
 
     @Test
