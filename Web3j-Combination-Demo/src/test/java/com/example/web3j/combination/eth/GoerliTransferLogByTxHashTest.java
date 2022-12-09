8e25932ed4be6525b2a0f745eb42075b94ffd0af
roylic <roylic@wokoworks.com>
2022-12-09 15:56:30 +0800

    adding txn for decode transaction fee

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/GoerliTransferLogByTxHashTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/GoerliTransferLogByTxHashTest.java
@@ -1,6 +1,7 @@
 package com.example.web3j.combination.eth;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StopWatch;
 import org.web3j.abi.EventEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
@@ -11,17 +12,17 @@ import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.request.EthFilter;
-import org.web3j.protocol.core.methods.response.EthBlock;
-import org.web3j.protocol.core.methods.response.EthLog;
-import org.web3j.protocol.core.methods.response.EthTransaction;
-import org.web3j.protocol.core.methods.response.Transaction;
+import org.web3j.protocol.core.methods.response.*;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.utils.Convert;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author Roylic
@@ -81,5 +82,87 @@ public class GoerliTransferLogByTxHashTest {
 
     }
 
+
+    @Test
+    public void getLogByTxHashReceiptTest() throws IOException {
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        String txHash = "0xd1feaef0379bbba0045ea8e2925877bc05ff1527d84399860a040ae4197c492e";
+
+        // find txn first
+        CompletableFuture<EthGetTransactionReceipt> txnReceiptFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return web3j.ethGetTransactionReceipt(txHash).send();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+
+        CompletableFuture<EthTransaction> txnFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                return web3j.ethGetTransactionByHash(txHash).send();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
+
+        try {
+            TransactionReceipt transactionReceipt = txnReceiptFuture.get().getTransactionReceipt().get();
+            Transaction transaction = txnFuture.get().getTransaction().get();
+            EthBlock ethBlock = web3j.ethGetBlockByHash(transactionReceipt.getBlockHash(), false).send();
+            BigInteger gasUsed = transactionReceipt.getGasUsed();
+            BigInteger transactionFee = transaction.getGasPrice().multiply(gasUsed);
+
+
+            // timestamp in second
+            BigInteger timestamp = ethBlock.getBlock().getTimestamp();
+
+            Event transfer = new Event("Transfer",
+                    Arrays.asList(
+                            TypeReference.create(Address.class, true),
+                            TypeReference.create(Address.class, true),
+                            // depends, erc-20 = value, erc-721 = tokenId
+                            TypeReference.create(Uint256.class))
+            );
+
+            EthFilter ethFilter = new EthFilter(
+                    DefaultBlockParameter.valueOf(transaction.getBlockNumber()),
+                    DefaultBlockParameter.valueOf(transaction.getBlockNumber()),
+                    "0xba62bcfcaafc6622853cca2be6ac7d845bc0f2dc");
+            ethFilter.addSingleTopic(EventEncoder.encode(transfer));
+
+            EthLog send = web3j.ethGetLogs(ethFilter).send();
+            send.getLogs().stream()
+                    .filter(sinEthLog -> ((EthLog.LogObject) sinEthLog).getTransactionHash().equals(transaction.getHash()))
+                    .forEach(sinEthLog -> {
+                        EthLog.LogObject curLogObj = (EthLog.LogObject) sinEthLog;
+                        List<String> topics = curLogObj.getTopics();
+                        String fromAddress = topics.get(1);
+                        String toAddress = topics.get(2);
+                        List<Type> decode = FunctionReturnDecoder.decode(curLogObj.getData(), transfer.getNonIndexedParameters());
+                        Uint256 val256 = (Uint256) decode.get(0);
+                        System.out.println("From Address: " + fromAddress);
+                        System.out.println("To Address: " + toAddress);
+                        System.out.println("Token Transferred: " + Convert.fromWei(val256.getValue().toString(), Convert.Unit.ETHER));
+                        System.out.println("Block Height: " + ethBlock.getBlock().getNumber());
+                        System.out.println("Block Hash: " + ethBlock.getBlock().getHash());
+                        System.out.println("Block Time: " + timestamp);
+                        System.out.println("Transaction Fee: " + new BigDecimal(transactionFee).divide(BigDecimal.TEN.pow(18)).toPlainString());
+                        System.out.println();
+                    });
+
+            stopWatch.stop();
+            System.out.println("PERFORMANCE: " + stopWatch);
+
+
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
