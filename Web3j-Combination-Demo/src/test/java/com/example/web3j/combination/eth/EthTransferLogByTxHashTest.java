e5640ac1871bc7e84a9a199b54f58a19e8b84f7e
roylic <roylic@wokoworks.com>
2023-01-04 13:47:48 +0800

    adding router for handling both ERC-20 Token transfer & Native Unit transfer decoding

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthTransferLogByTxHashTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthTransferLogByTxHashTest.java
@@ -26,6 +26,7 @@ import java.math.BigInteger;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Roylic
@@ -189,6 +190,111 @@ public class EthTransferLogByTxHashTest {
         System.out.println("Block Time: " + ethBlock.getBlock().getTimestamp());
         System.out.println("Transaction Fee: " + new BigDecimal(transactionFee).divide(BigDecimal.TEN.pow(18)).toPlainString());
         System.out.println();
+    }
+
+
+    @Test
+    public void routerByTxHashTest() throws IOException {
+
+        // native
+//        String txHash = "0x345317d96c11222fb1b92ade51f8157b9962138343a3c842729d925f02f27dd1";
+
+        // erc-20
+        String txHash = "0x49e75311432d87747841c7de654bd2df973464ed90c3945e2e4974d8002810c1";
+
+
+        Transaction transaction = web3j.ethGetTransactionByHash(txHash).send().getTransaction().get();
+        TransactionReceipt transactionReceipt = web3j.ethGetTransactionReceipt(txHash).send().getTransactionReceipt().get();
+        EthBlock ethBlock = web3j.ethGetBlockByHash(transactionReceipt.getBlockHash(), false).send();
+        BigInteger gasUsed = transactionReceipt.getGasUsed();
+        BigInteger transactionFee = transaction.getGasPrice().multiply(gasUsed);
+
+
+        // separate into native token transfer or ERC-20 transfer
+        boolean isNative = false;
+        AtomicReference<String> fromAddress = new AtomicReference<>(transaction.getFrom());
+        AtomicReference<String> toAddress = new AtomicReference<>(transaction.getTo());
+        String blockNum = ethBlock.getBlock().getNumber().toString();
+        String blockHash = ethBlock.getBlock().getHash();
+        String blockTimestamp = ethBlock.getBlock().getTimestamp().toString();
+        AtomicReference<String> transferredVal = new AtomicReference<>("");
+        AtomicReference<String> decimal = new AtomicReference<>("");
+
+        BigDecimal txnVal = Convert.fromWei(transaction.getValue().toString(), Convert.Unit.ETHER);
+        if (txnVal.compareTo(BigDecimal.ZERO) > 0) {
+            // native transaction
+            isNative = true;
+            transferredVal.set(txnVal.stripTrailingZeros().toPlainString());
+
+        } else {
+            // erc-20 transaction
+            isNative = false;
+            // get log
+            Event transfer = new Event("Transfer",
+                    Arrays.asList(
+                            TypeReference.create(Address.class, true),
+                            TypeReference.create(Address.class, true),
+                            // depends, erc-20 = value, erc-721 = tokenId
+                            TypeReference.create(Uint256.class))
+            );
+            EthFilter ethFilter = new EthFilter(
+                    DefaultBlockParameter.valueOf(transaction.getBlockNumber()),
+                    DefaultBlockParameter.valueOf(transaction.getBlockNumber()),
+                    transaction.getTo());
+            ethFilter.addSingleTopic(EventEncoder.encode(transfer));
+
+            // filter log
+            EthLog ethLog = web3j.ethGetLogs(ethFilter).send();
+            ethLog.getLogs().stream()
+                    .filter(sinEthLog -> ((EthLog.LogObject) sinEthLog).getTransactionHash().equals(transaction.getHash()))
+                    .forEach(sinEthLog -> {
+                        EthLog.LogObject curLogObj = (EthLog.LogObject) sinEthLog;
+                        List<String> topics = curLogObj.getTopics();
+                        fromAddress.set(new Address(topics.get(1)).toString());
+                        toAddress.set(new Address(topics.get(2)).toString());
+                        List<Type> decode = FunctionReturnDecoder.decode(curLogObj.getData(), transfer.getNonIndexedParameters());
+                        Uint256 val256 = (Uint256) decode.get(0);
+
+                        try {
+
+                            // from decimal
+                            Function decimalFunc = new Function("decimals", Collections.emptyList(),
+                                    Collections.singletonList(TypeReference.create(Uint8.class)));
+
+                            String decimalFuncSignature = FunctionEncoder.encode(decimalFunc);
+                            org.web3j.protocol.core.methods.request.Transaction reqTxn =
+                                    org.web3j.protocol.core.methods.request.Transaction.createEthCallTransaction(
+                                            transaction.getTo(), transaction.getTo(), decimalFuncSignature);
+
+                            String beforeDecode = web3j.ethCall(reqTxn, DefaultBlockParameterName.LATEST).send().getValue();
+                            List<Type> decoded = FunctionReturnDecoder.decode(beforeDecode, decimalFunc.getOutputParameters());
+                            Uint8 decimals = (Uint8) decoded.get(0);
+                            decimal.set(decimals.getValue().toString());
+
+                            // convert and set token transferred
+                            transferredVal.set(val256.getValue().divide(decimals.getValue()).toString());
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+
+
+        System.out.println("From Address: " + fromAddress);
+        System.out.println("To Address: " + toAddress);
+        System.out.println("Native or Token: " + (isNative ? "Native" : "Token"));
+        System.out.println("Transferred: " + txnVal);
+        if (!isNative) {
+            System.out.println("Decimals: " + decimal);
+        }
+        System.out.println("Block Height: " + blockNum);
+        System.out.println("Block Hash: " + blockHash);
+        System.out.println("Block Time: " + blockTimestamp);
+        System.out.println("Transaction Fee: " + new BigDecimal(transactionFee).divide(BigDecimal.TEN.pow(18)).toPlainString());
+        System.out.println();
+
+
     }
 
 
