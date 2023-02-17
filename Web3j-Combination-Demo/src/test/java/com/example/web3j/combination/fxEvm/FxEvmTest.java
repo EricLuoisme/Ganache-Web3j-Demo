cb9bf8f4320b6fea0d41252fe7bc32b5e9205e5e
roylic <roylic@wokoworks.com>
2023-02-17 16:13:19 +0800

    correct the token decoding stuff on fx-evm

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/fxEvm/FxEvmTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/fxEvm/FxEvmTest.java
@@ -1,16 +1,26 @@
 package com.example.web3j.combination.fxEvm;
 
+import com.example.web3j.combination.web3j.EthLogConstants;
 import com.example.web3j.combination.web3j.handler.NftUriDecodeHandler;
 import org.junit.jupiter.api.Test;
+import org.web3j.abi.FunctionReturnDecoder;
+import org.web3j.abi.Utils;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Type;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.request.EthFilter;
 import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthLog;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Roylic
@@ -43,6 +53,51 @@ public class FxEvmTest {
         EthBlock block_5528640 = web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(new BigInteger("5528640")), false).send();
         System.out.println(block_5528640.toString());
     }
+
+
+    @Test
+    public void get721Log() throws IOException {
+
+        EthFilter filter = new EthFilter(
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(8679536)),
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(8679536)),
+                Collections.singletonList("0x9E4df6f08ceEcfEF170FCbF036B97789d5320ec3"));
+
+        filter.addOptionalTopics(EthLogConstants.EthEventTopics.getTopicStr(EthLogConstants.EthEventTopics.TRANSFER_TOPIC_ERC_20_721));
+        EthLog logs = web3j.ethGetLogs(filter).send();
+
+        logs.getLogs().forEach(sinEthLog -> {
+
+            EthLog.LogObject curLogObj = (EthLog.LogObject) sinEthLog;
+
+            // filter target txn
+            if (!"0x67909fba927f5171ba7f85c8d7acd247290c5497aa7038a2d1b0622784e9c567".equals(curLogObj.getTransactionHash())) {
+                return;
+            }
+
+            Iterator<String> iterator = curLogObj.getTopics().iterator();
+
+            // 1. signature
+            iterator.next();
+
+            // 2. from address
+            String from = new Address(iterator.next()).getValue();
+
+            // 3. to address
+            String to = new Address(iterator.next()).getValue();
+
+            // 4. tokenId
+            String tokenStr = iterator.next();
+            List<Type> valueDecodeList = FunctionReturnDecoder.decode(tokenStr, Utils.convert(EthLogConstants.EthFuncOutput.UINT256.getFuncOutputParams()));
+            BigDecimal tokenId = new BigDecimal((BigInteger) valueDecodeList.get(0).getValue());
+
+            System.out.println("From address " + from);
+            System.out.println("To address " + to);
+            System.out.println("Token id " + tokenId);
+        });
+
+    }
+
 
     @Test
     public void FoxContractNftDecoding() throws IOException {
