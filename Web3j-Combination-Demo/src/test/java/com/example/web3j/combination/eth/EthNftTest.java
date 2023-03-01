f7a87f80cf34e6596a974b8a1bb64bf3075db31a
roylic <roylic@wokoworks.com>
2023-03-01 16:44:35 +0800

    find out just because the contract not output any url info

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthNftTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthNftTest.java
@@ -1,18 +1,16 @@
 package com.example.web3j.combination.eth;
 
+import com.example.web3j.combination.web3j.EthLogConstants;
 import org.junit.jupiter.api.Test;
-import org.web3j.abi.EventEncoder;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.FunctionReturnDecoder;
-import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Event;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.*;
+import org.web3j.abi.datatypes.*;
 import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.core.methods.response.EthLog;
 import org.web3j.protocol.http.HttpService;
 
@@ -115,6 +113,53 @@ public class EthNftTest {
         System.out.println("Drop Stage Index: " + dropStageIndex.getValue());
 
         System.out.println();
+    }
+
+    @Test
+    public void urlCalling() throws IOException {
+
+        EthFilter filter = new EthFilter(
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(8570683)),
+                DefaultBlockParameter.valueOf(BigInteger.valueOf(8570683)),
+                Collections.emptyList());
+
+        filter.addOptionalTopics(EthLogConstants.EthEventTopics.getTopicStr(EthLogConstants.EthEventTopics.TRANSFER_TOPIC_ERC_20_721));
+        EthLog logs = web3j.ethGetLogs(filter).send();
+        List<EthLog.LogResult> collect = logs.getLogs().stream()
+                .filter(log -> "0x0e71420f7b965345248f33764327619ea5680356fd1d87754a9f9be00be37aeb"
+                        .equals(((EthLog.LogObject) log).getTransactionHash()))
+                .collect(Collectors.toList());
+
+        EthLog.LogObject logResult = (EthLog.LogObject) collect.get(0);
+        List<String> topics = logResult.getTopics();
+
+        List<TypeReference<?>> unit256 = Collections.singletonList(TypeReference.create(Uint256.class));
+        List<Type> tokenIdDecode = FunctionReturnDecoder.decode(topics.get(3), Utils.convert(unit256));
+        Uint256 tokenId = (Uint256) tokenIdDecode.get(0);
+
+
+        String contract = "0xD5835369d4F691094D7509296cFC4dA19EFe4618";
+        // construct for func + params
+        Function tokenUriFunc = new Function(
+                "tokenURI",
+                Collections.singletonList(tokenId),
+                Collections.singletonList(TypeReference.create(Utf8String.class))
+        );
+
+        String encode = FunctionEncoder.encode(tokenUriFunc);
+        System.out.println("ERC-721-Code >>> " + encode.substring(0, 10));
+
+        // call contract for url
+        Transaction reqTxn = Transaction.createEthCallTransaction(contract, contract, encode);
+        EthCall callResult = web3j.ethCall(reqTxn, DefaultBlockParameterName.LATEST).send();
+        List<Type> tokenUriDecoded = FunctionReturnDecoder.decode(callResult.getValue(), tokenUriFunc.getOutputParameters());
+
+        // decode token url
+        Utf8String tokenBaseUrl = (Utf8String) tokenUriDecoded.get(0);
+        System.out.println("ERC-721 token base uri >>> " + tokenBaseUrl.getValue());
+
+        // replace {id} with real id, for this OPEN_SEA's contract, we need to convert tokenId to hex
+        System.out.println("Token Id in hex >>> " + tokenId.getValue());
     }
 
 
