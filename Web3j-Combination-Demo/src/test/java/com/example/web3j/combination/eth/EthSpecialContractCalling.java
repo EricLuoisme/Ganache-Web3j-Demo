a7473a0eceb14be0878184728b6cb37198389304
roylic <roylic@wokoworks.com>
2023-03-03 16:28:05 +0800

    adding DynamicType for the receiving & adding test

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthSpecialContractCalling.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/eth/EthSpecialContractCalling.java
@@ -1,13 +1,12 @@
 package com.example.web3j.combination.eth;
 
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
-import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Bool;
-import org.web3j.abi.datatypes.Function;
-import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.*;
+import org.web3j.abi.datatypes.generated.Uint8;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.Hash;
 import org.web3j.crypto.RawTransaction;
@@ -38,13 +37,16 @@ public class EthSpecialContractCalling {
 
     public static final String contractAdd = "0xa9E628B29169ef448dBf362ec068EC1F414505BC";
 
-    String PRI_KEY = "";
+    public static final String supportTokenAddress = "0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc";
+
+    public static final String marketMakerAddress = "0x36F0A040C8e60974d1F34b316B3e956f509Db7e5";
+
+    public static final String PRI_KEY = "";
 
 
     @Test
     public void marketMakerValidationTest() throws IOException {
         // encode
-        String marketMakerAddress = "0x36F0A040C8e60974d1F34b316B3e956f509Db7e5";
         Function marketMakerFunc = new Function(
                 "marketMaker",
                 Collections.singletonList(new Address(marketMakerAddress)),
@@ -62,27 +64,57 @@ public class EthSpecialContractCalling {
         System.out.println(returnVal.getValue());
     }
 
-
-
-
     @Test
     public void marketMakerRegistrationTest() throws IOException {
         // encode input data
-        String marketMakerAddress = "0x36F0A040C8e60974d1F34b316B3e956f509Db7e5";
         Function registerMarketMaker = new Function(
                 "registerMarketMaker",
                 Collections.singletonList(new Address(marketMakerAddress)),
                 Collections.singletonList(TypeReference.create(Bool.class)));
         String data = FunctionEncoder.encode(registerMarketMaker);
         // construct txn
-        constructAndCallingContractFunction(data);
+        constructAndCallingContractFunction(data, PRI_KEY);
     }
+
+    @Test
+    public void marketMakerTokenAddTest() throws IOException {
+        // encode input data
+        Function marketMakerAddTokenFunc = new Function(
+                "marketMakerAddToken",
+                Collections.singletonList(new Address(supportTokenAddress)),
+                Collections.singletonList(TypeReference.create(Bool.class)));
+        String data = FunctionEncoder.encode(marketMakerAddTokenFunc);// construct txn
+        constructAndCallingContractFunction(data, PRI_KEY);
+    }
+
+
+    @Test
+    public void supportTokenCheckingTest() throws IOException {
+        /// encode
+        Function marketMakerTokenFunc = new Function(
+                "getMarketMakerToken",
+                Collections.singletonList(new Address(marketMakerAddress)),
+                Collections.singletonList(new TypeReference<DynamicArray<PaymentToken>>() {
+                }));
+
+        String encode = FunctionEncoder.encode(marketMakerTokenFunc);
+        System.out.println("Market Maker Func coding: " + encode);
+
+        // call contract
+        Transaction mmCallingTxn = Transaction.createEthCallTransaction(contractAdd, contractAdd, encode);
+        EthCall send = web3j.ethCall(mmCallingTxn, DefaultBlockParameterName.LATEST).send();
+
+        // decode
+        List<Type> decode = FunctionReturnDecoder.decode(send.getValue(), marketMakerTokenFunc.getOutputParameters());
+        System.out.println();
+    }
+
 
     /**
      * Construct txn inputs & execute
      */
-    private void constructAndCallingContractFunction(String data) throws IOException {
-        Credentials credentials = Credentials.create(PRI_KEY);
+    private void constructAndCallingContractFunction(String data, String priKey) throws IOException {
+        Credentials credentials = Credentials.create(priKey);
         EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(credentials.getAddress(), DefaultBlockParameterName.LATEST).send();
         BigInteger nonce = ethGetTransactionCount.getTransactionCount();
         // another stuff need to be filled
@@ -100,6 +132,22 @@ public class EthSpecialContractCalling {
         System.out.println("OffChain txHash: " + txHash);
         EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send();
         System.out.println("OnChain txHash: " + ethSendTransaction.getTransactionHash());
+    }
+
+    @Data
+    public class PaymentToken extends DynamicStruct {
+        private Address address;
+        private Utf8String name;
+        private Utf8String symbol;
+        private Uint8 decimals;
+
+        public PaymentToken(Address address, Utf8String name, Utf8String symbol, Uint8 decimals) {
+            super(address, name, symbol, decimals);
+            this.address = address;
+            this.name = name;
+            this.symbol = symbol;
+            this.decimals = decimals;
+        }
     }
 
 }
