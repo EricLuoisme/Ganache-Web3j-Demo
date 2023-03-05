2d560f12a69d2db523df23111f8b7aede3866059
roylic <lykismekk@outlook.com>
2023-03-05 13:50:02 +0800

    adding explanation after reading related articles for Solana

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/solana/SolanaProgramRelated.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/solana/SolanaProgramRelated.java
@@ -11,6 +11,13 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Solana related tests
+ * <p>
+ * - programs do not contain any data, but instead reference accounts
+ * where data can be stored. This enables programs to run concurrently,
+ * as long as they are not accessing the same account data
+ * <p>
+ * - No need to deploy a new program for each new token -> a standard Token Program
+ * already deployed and ready for anyone to use for creating, minting, trading, burning
  *
  * @author Roylic
  * 2022/10/10
@@ -20,8 +27,9 @@ public class SolanaProgramRelated {
 
     private static final String SOLANA_DEV_URL = HttpUrl.parse("https://solana-devnet.g.alchemy.com/v2/On35d8LdFc1QGYD-wCporecGj359qian").newBuilder().build().toString();
     private static final MediaType mediaType = MediaType.parse("application/json");
-    private static final String ADDRESS = "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D";
-    private static final String TOKEN_ADDRESS = "Gd8nxWzbnJ2zwtn5TukvEMKKjjbFhdtqA1L67DgnRvXc";
+    private static final String ACCOUNT = "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D";
+    private static final String TOKEN_ACCOUNT = "Gd8nxWzbnJ2zwtn5TukvEMKKjjbFhdtqA1L67DgnRvXc";
+    private static final String PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 
 
     // could not be used with HTTPS
@@ -35,15 +43,25 @@ public class SolanaProgramRelated {
 
     @Test
     public void getBalance() throws IOException {
-        String getBalance = "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"getBalance\", \"params\":[\"" + ADDRESS + "\"]}";
+        String getBalance = "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"getBalance\", \"params\":[\"" + ACCOUNT + "\"]}";
         callAndPrint(getBalance);
     }
 
-    // means to check the token's program remaining token amount
+    // means to check the Address's token account remaining token amount
     @Test
     public void getTokenAccountBalance() throws IOException {
-        String getTokenAccountBalance = "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"getTokenAccountBalance\", \"params\":[\"" + TOKEN_ADDRESS + "\"]}";
+        String getTokenAccountBalance = "{\"jsonrpc\":\"2.0\", \"id\":1, \"method\":\"getTokenAccountBalance\"," +
+                " \"params\":[\"" + TOKEN_ACCOUNT + "\"]}";
         callAndPrint(getTokenAccountBalance);
+    }
+
+    // mean to check account-address's remaining tokens in that specific program with program id
+    @Test
+    public void getTokenAccountsByOwner() throws IOException {
+        String getAddressRemainingToken = "{\"jsonrpc\":\"2.0\", \"id\":1, " +
+                "\"method\":\"getTokenAccountsByOwner\", \"params\":[\"" + TOKEN_ACCOUNT + "\", " +
+                "{\"programId\":\"" + PROGRAM_ID + "\"}]}";
+        callAndPrint(getAddressRemainingToken);
     }
 
 
