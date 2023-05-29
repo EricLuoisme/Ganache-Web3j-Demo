6fc72049a2509c1e8debe1988a3ad41e2d859268
roylic <lykismekk@outlook.com>
2023-05-29 23:26:36 +0800

    tmp commit for invalid grpc calling

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/cosmos/CosmosConnectionTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/cosmos/CosmosConnectionTest.java
@@ -1,8 +1,14 @@
 package com.example.web3j.combination.cosmos;
 
+import cosmos.base.tendermint.v1beta1.Query;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,7 +27,23 @@ public class CosmosConnectionTest {
             .build();
 
     @Test
-    public void connectionTest() {
+    public void connectionTest() throws IOException {
+
+        ManagedChannel build = ManagedChannelBuilder.forAddress("https://testnet-fx-grpc.functionx.io", 9090)
+                .usePlaintext()
+                .build();
+
+        Request req = new Request.Builder()
+                .url("https://testnet-fx-grpc.functionx.io:9090/status")
+                .get()
+                .build();
+
+        Response execute = okHttpClient.newCall(req).execute();
+        System.out.println();
+
+
+        Query.GetLatestBlockRequest request = Query.GetLatestBlockRequest.newBuilder().build();
+
 
     }
 
