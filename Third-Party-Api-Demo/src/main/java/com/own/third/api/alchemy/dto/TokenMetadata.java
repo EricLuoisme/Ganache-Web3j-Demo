5df10a39faf4b8e3331028a788d75071cf6b214c
roylic <lykismekk@outlook.com>
2023-07-12 22:26:15 +0800

    adding metadata request

---
+++ Third-Party-Api-Demo/src/main/java/com/own/third/api/alchemy/dto/TokenMetadata.java
@@ -0,0 +1,11 @@
+package com.own.third.api.alchemy.dto;
+
+import lombok.Data;
+
+@Data
+public class TokenMetadata {
+    private Integer decimals;
+    private String logo;
+    private String name;
+    private String symbol;
+}
