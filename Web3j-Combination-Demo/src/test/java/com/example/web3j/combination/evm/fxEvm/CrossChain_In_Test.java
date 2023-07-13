8ed5f6064a83c9269628caa219b754677f861dd7
roylic <lykismekk@outlook.com>
2023-07-13 23:31:50 +0800

    successfully execute the Polygon mumbai contract calling -> send2Fx contract execution

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/evm/fxEvm/CrossChain_In_Test.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/evm/fxEvm/CrossChain_In_Test.java
@@ -7,13 +7,17 @@ import org.web3j.abi.*;
 import org.web3j.abi.datatypes.*;
 import org.web3j.abi.datatypes.generated.Bytes32;
 import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.*;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.EthFilter;
-import org.web3j.protocol.core.methods.response.EthLog;
-import org.web3j.protocol.core.methods.response.EthTransaction;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.*;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
 import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -24,6 +28,9 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+
+import static com.example.web3j.combination.evm.fxEvm.CrossChain_Out_Test.loadBip44Mnemonic2Credential;
+import static com.example.web3j.combination.evm.fxEvm.CrossChain_Out_Test.strToLittleEndianBytes32;
 
 /**
  * @author Roylic
@@ -142,6 +149,78 @@ public class CrossChain_In_Test {
         System.out.println();
     }
 
+    @Test
+    public void send2FxTxnTest() throws IOException {
+
+        String mnemonic = "";
+        String password = "";
+        Credentials credential = loadBip44Mnemonic2Credential(mnemonic, password, 2);
+        System.out.println("Derived address: " + credential.getAddress());
+
+        String sender = "0x36F0A040C8e60974d1F34b316B3e956f509Db7e5";
+        String tokenContract = "0x40212e7bb7f79fe7ff3870cbd55427c01d69d2b7";
+        String crossBridgeContract = "0x57b1e4c85b0f141ade38b5573907ba8ef9ac2298";
+
+        // construct txn
+        Function send2FxFunc = new Function("sendToFx",
+                Arrays.asList(
+                        new Address(tokenContract), // token contract
+                        new Bytes32(Numeric.hexStringToByteArray("00000000000000000000000036f0a040c8e60974d1f34b316b3e956f509db7e5")), // destination
+                        new Bytes32(strToLittleEndianBytes32("erc20")), // targetIBC
+                        new Uint256(new BigInteger("3290000")) // amount
+                ),
+                Collections.singletonList(TypeReference.create(Bool.class)));
+
+        String data = FunctionEncoder.encode(send2FxFunc);
+        System.out.println("send2FxFunc function encoded data: " + data);
+
+        // native balance
+        EthGetBalance send = web3j.ethGetBalance(sender, DefaultBlockParameterName.LATEST).send();
+        System.out.println("balance: " + send.getBalance());
+
+        // token balance
+        Function balanceOfFunc = new Function("balanceOf",
+                Collections.singletonList(new Address(sender)),
+                Collections.singletonList(TypeReference.create(Uint256.class)));
+        String balanceFuncEnc = FunctionEncoder.encode(balanceOfFunc);
+
+        Transaction ethCallTransaction = Transaction.createEthCallTransaction(tokenContract, tokenContract, balanceFuncEnc);
+        EthCall ethCallResp = web3j.ethCall(ethCallTransaction, DefaultBlockParameterName.LATEST).send();
+        List<Type> decodeList = FunctionReturnDecoder.decode(ethCallResp.getValue(), balanceOfFunc.getOutputParameters());
+        System.out.println("Token Balance: " + ((Uint256) decodeList.get(0)).getValue());
+
+        // call contract
+        constructAndCallingContractFunction(sender, data, crossBridgeContract, credential);
+    }
+
+    /**
+     * Construct txn inputs & execute, for some reason, the nonce could not be correctly get from web3j.ethGetTransactionCount
+     */
+    private void constructAndCallingContractFunction(String senderAddress, String data, String callingContract, Credentials credentials) throws IOException {
+        // another stuff need to be filled
+        long chainId = 80001; // for polygon mumbai
+
+        // get nonce
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(senderAddress, DefaultBlockParameterName.LATEST).send();
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+
+        BigInteger gasPrice = Convert.toWei("1", Convert.Unit.GWEI).toBigInteger();
+        BigInteger gasLimit = new BigInteger("250000");
+
+        // for interact with contract, value have to input 0
+        BigInteger value = BigInteger.valueOf(0L);
+
+        RawTransactionManager rawTransactionManager = new RawTransactionManager(web3j, credentials, chainId);
+        EthSendTransaction ethSendTransaction = rawTransactionManager.sendTransaction(gasPrice, gasLimit, callingContract, data, value);
+
+        if (ethSendTransaction.hasError()) {
+            System.out.println("Error received: " + ethSendTransaction.getError().getMessage());
+        } else {
+            System.out.println("OnChain txHash: " + Keys.toChecksumAddress(ethSendTransaction.getTransactionHash()));
+            EthGetTransactionReceipt receiptResp = web3j.ethGetTransactionReceipt(ethSendTransaction.getTransactionHash()).send();
+            System.out.println(receiptResp.getTransactionReceipt());
+        }
+    }
 
     @NotNull
     private String littleEndianByte32ArrToStr(byte[] bytesArr) {
