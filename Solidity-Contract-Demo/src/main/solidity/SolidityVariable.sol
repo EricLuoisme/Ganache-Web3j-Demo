f79069b67d6dadc940e2102e2ec6d0f60318749c
roylic <lykismekk@outlook.com>
2023-07-25 23:19:45 +0800

    update variable scope

--- Solidity-Contract-Demo/src/main/solidity/SolidityVariable.sol
+++ Solidity-Contract-Demo/src/main/solidity/SolidityVariable.sol
@@ -3,16 +3,18 @@ pragma solidity >=0.5.0;
 
 contract SolidityVariable {
     uint storedData; // state variable
+    uint public count = 2; // public data, would generate get function automatically
+    uint internal iCount = 1; // internal data, only can be accessed by contract (when it's running)
+
     constructor() public {
         // using state variable
         storedData = 10;
     }
-    function getResult() public pure returns (uint) {
-        // local variable
+    function getResult() public view returns (uint) {
+        // local variable (also private variable)
         uint a = 1;
         uint b = 2;
-        uint result = a + b;
-        // access local variable
+        uint result = a + b + storedData;
         return result;
     }
     // use globally available variables
