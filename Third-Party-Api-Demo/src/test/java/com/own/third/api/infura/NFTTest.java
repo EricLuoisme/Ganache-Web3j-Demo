f424cfd6aa9fbb106fd21c9f800d127da2624d53
roylic <roylic@wokoworks.com>
2023-07-25 10:43:32 +0800

    tmp cmt

--- Third-Party-Api-Demo/src/test/java/com/own/third/api/infura/NFTTest.java
+++ Third-Party-Api-Demo/src/test/java/com/own/third/api/infura/NFTTest.java
@@ -3,10 +3,7 @@ package com.own.third.api.infura;
 import com.alibaba.fastjson2.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.own.third.api.TrustAllX509CertManager;
-import okhttp3.Credentials;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -44,19 +41,25 @@ public class NFTTest {
     @Test
     public void getAllNftOwnedByAddress() throws IOException {
 
-        String owner = "0x70076F9f8e221d4729314f99a8AB410C117560aB";
+        String owner = "";
         Long chainId = 97L;
 
+        String cursor = "eyJhbGciOiJIUzI1NiJ9.ZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpkWE4wYjIxUVlYSmhiWE1pT25zaWQyRnNiR1YwUVdSa2NtVnpjeUk2SWpCNE56ZG1NakF5TWpVek1qQXdPV00xWldJMFl6WmpOekJtTXprMVpHVmhZV0UzT1RNME9ERmlZeUo5TENKclpYbHpJanBiSWpFMk5ESTFNRGt6TURFdU5UZzFJbDBzSW5kb1pYSmxJanA3SW05M2JtVnlYMjltSWpvaU1IZzNOMll5TURJeU5UTXlNREE1WXpWbFlqUmpObU0zTUdZek9UVmtaV0ZoWVRjNU16UTRNV0pqSW4wc0lteHBiV2wwSWpveE1EQXNJbTltWm5ObGRDSTZNQ3dpYjNKa1pYSWlPbHRkTENKa2FYTmhZbXhsWDNSdmRHRnNJanBtWVd4elpTd2lkRzkwWVd3aU9qUXpOU3dpY0dGblpTSTZNU3dpZEdGcGJFOW1abk5sZENJNk1Td2lhV0YwSWpveE5qa3dNVGsxTmpRd2ZRLkpLamJ6UTFTbUZXNzFaRHlkel9tLWt6VndWSjB2UXIxRS10NG5DTXg5UWc.wOgo9rWXDLADbydParU7L-cINbhqx5Z0VeuaJinmfDA";
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(String.format(NFT_REQUEST_URL, chainId, owner)).newBuilder();
+        urlBuilder.addQueryParameter("cursor", cursor);
+        String url = urlBuilder.build().toString();
+
         Request request = new Request.Builder()
-                .url(String.format(NFT_REQUEST_URL, chainId, owner))
+                .url(url)
                 .addHeader("accept", "application/json")
-//                .addHeader("Authorization", CREDENTIAL)
                 .addHeader("Authorization", "Basic " + Base64.getEncoder().encodeToString(COMPACT_CREDENTIAL.getBytes(StandardCharsets.UTF_8)))
                 .build();
 
         Response response = okHttpClient.newCall(request).execute();
         NftResponse nftResponse = JSON.parseObject(response.body().string(), NftResponse.class);
-        System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(nftResponse));System.out.println();
+        System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(nftResponse));
+        System.out.println();
     }
 
 }
