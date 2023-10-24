3af0fafb78198ed6d121596a02bb24c3801e0198
roylic <lykismekk@outlook.com>
2023-10-24 13:28:21 +0800

    add nft tokenUri request - some bsc contract's memory is empty now, legacy nfts can not be decoding

--- Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/evm/bsc/BscConnectingTest.java
+++ Web3j-Combination-Demo/src/test/java/com/example/web3j/combination/evm/bsc/BscConnectingTest.java
@@ -1,5 +1,6 @@
 package com.example.web3j.combination.evm.bsc;
 
+import com.example.web3j.combination.web3j.handler.NftUriDecodeHandler;
 import org.junit.jupiter.api.Test;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
@@ -8,6 +9,7 @@ import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 
 import java.io.IOException;
+import java.util.Collections;
 
 /**
  * 链接bsc节点的测试
@@ -19,7 +21,8 @@ public class BscConnectingTest {
 
     public static final Web3j web3j
 //            = Web3j.build(new HttpService("https://data-seed-prebsc-1-s1.binance.org:8545/"));
-            = Web3j.build(new HttpService("http://testnet-bsc-dataseed1.functionx.io:8545/"));
+//            = Web3j.build(new HttpService("http://testnet-bsc-dataseed1.functionx.io:8545/"));
+            = Web3j.build(new HttpService("https://bsc-dataseed1.binance.org/"));
 
     @Test
     public void testConnection() {
@@ -40,6 +43,16 @@ public class BscConnectingTest {
         EthBlock.Block block = web3j.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false)
                 .send().getBlock();
         System.out.println(block.getNumber().toString());
+    }
+
+    @Test
+    public void getNftTokenUri() {
+
+        String contract = "0xCAd145eFC1eB20EB896b8F80e03D4d081Dd46D44";
+        String tokenIdStr = "1";
+        String tokenBaseUri = NftUriDecodeHandler.tokenBaseUriRetrieving(
+                web3j, contract, Collections.singletonList(tokenIdStr), NftUriDecodeHandler.SupportErc.ERC_721);
+        System.out.println(tokenBaseUri);
     }
 
 
