095ee19f01bfc19f53af182dfe9e81848245fa22
roylic <35546172+EricLuoisme@users.noreply.github.com>
2023-11-22 14:45:59 +0800

    add simple demo for using Java stream's grouping by functionality

---
+++ Programmer-Trick-Demo/src/test/java/com/example/trick/stream/GroupingByDemo.java
@@ -0,0 +1,32 @@
+package com.example.trick.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class GroupingByDemo {
+
+    public static void main(String[] args) {
+        List<Item> items = Arrays.asList(
+                new Item("key1", "value1"), new Item("key2", "value2"),
+                new Item("key1", "value3"), new Item("key2", "value4"),
+                new Item("key1", "value1"));
+
+        // by using groupingBy + mapping, we could better traverse & collect the values
+        Map<String, Set<String>> collect = items.stream()
+                .collect(
+                        Collectors.groupingBy(item -> item.key,
+                                Collectors.mapping(item -> item.value, Collectors.toSet())));
+        System.out.println();
+    }
+
+    // Example class
+    static class Item {
+        String key;
+        String value;
+
+        Item(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
