2b284f192a69abfdb5073024db4bebd64a93b283
Roylic Luo <35546172+EricLuoisme@users.noreply.github.com>
2023-12-08 10:23:19 +0800

    test: add info related unit tests

---
+++ Third-Party-Api-Demo/src/test/java/com/own/third/api/binance/BinanceInfoAPITest.java
@@ -0,0 +1,61 @@
+package com.own.third.api.binance;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.binance.connector.client.impl.SpotClientImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinanceInfoAPITest {
+
+
+    private final ObjectMapper om = new ObjectMapper();
+
+
+    private static final String TEST_URL = "https://testnet.binance.vision";
+    private static final String TEST_API = "";
+    private static final String TEST_SRC = "";
+
+    private static final SpotClientImpl TESTNET_SPOT_CLIENT = new SpotClientImpl(TEST_API, TEST_SRC, TEST_URL);
+
+    private static final SpotClientImpl MAINNET_SPOT_CLIENT = new SpotClientImpl();
+
+
+    @Test
+    public void testConnection() {
+        String ping = TESTNET_SPOT_CLIENT.createMarket().ping();
+        System.out.println(ping);
+    }
+
+    @Test
+    public void testAccInfo() throws JsonProcessingException {
+        Map<String, Object> reqMap = new HashMap<>();
+        reqMap.put("timestamp", Instant.now().toEpochMilli());
+        String accountInfoResp = TESTNET_SPOT_CLIENT.createTrade().account(reqMap);
+        JSONObject jsonObject = JSONObject.parseObject(accountInfoResp);
+        System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject));
+    }
+
+    @Test
+    public void testTradeInfo() throws JsonProcessingException {
+        String resp = MAINNET_SPOT_CLIENT.createMarket().exchangeInfo(Collections.emptyMap());
+        JSONObject jsonObject = JSONObject.parseObject(resp);
+        System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject));
+    }
+
+    @Test
+    public void specificTradePairInfo() throws JsonProcessingException {
+        Map<String, Object> reqMap = new HashMap<>();
+        reqMap.put("symbol", "BTCUSDT");
+        String resp = TESTNET_SPOT_CLIENT.createMarket().exchangeInfo(reqMap);
+        JSONObject jsonObject = JSONObject.parseObject(resp);
+        System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject));
+    }
+
+
+}
