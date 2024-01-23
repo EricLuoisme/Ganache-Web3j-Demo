86f92a77d3430bda30de2c9833c26972ff949a18
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-01-23 14:52:20 +0800

    feat: successfully go through the oauth1.0a process of Twitter

--- Third-Party-Api-Demo/src/test/java/com/own/third/api/twitter/TwitterAPITest_oauth1.java
+++ Third-Party-Api-Demo/src/test/java/com/own/third/api/twitter/TwitterAPITest_oauth1.java
@@ -1,67 +1,113 @@
 package com.own.third.api.twitter;
 
-import com.github.scribejava.core.model.OAuth2AccessToken;
-import com.github.scribejava.core.pkce.PKCE;
-import com.github.scribejava.core.pkce.PKCECodeChallengeMethod;
-import com.twitter.clientlib.TwitterCredentialsBearer;
-import com.twitter.clientlib.TwitterCredentialsOAuth2;
-import com.twitter.clientlib.auth.TwitterOAuth20Service;
+import okhttp3.*;
 import org.junit.jupiter.api.Test;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.RequestToken;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class TwitterAPITest_oauth1 {
 
-    private static final String BEARER = "";
+    private static final String baseAuthUrl = "https://api.twitter.com/oauth";
 
-    private static final String CLIENT_ID = "";
+    private static final String CONSUMER_K = "";
 
-    private static final String CLIENT_S = "";
+    private static final String CONSUMER_S = "";
 
-    private static final TwitterCredentialsBearer bearerCredential = new TwitterCredentialsBearer(BEARER);
+    private final MediaType mediaType = MediaType.parse("application/json");
 
-    private static final TwitterCredentialsOAuth2 oauth2Credential = new TwitterCredentialsOAuth2(
-            CLIENT_ID, CLIENT_S, "", "");
+    private static final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .build();
 
     @Test
-    public void oauth2Process() {
-        // 1. get oauth 2 access token
-        OAuth2AccessToken accessToken = getAccessToken(oauth2Credential);
-    }
+    public void oauth1_accTokenWholeProcess() {
 
+        Twitter twitterIns = new TwitterFactory().getInstance();
+        twitterIns.setOAuthConsumer(CONSUMER_K, CONSUMER_S);
 
-    public static OAuth2AccessToken getAccessToken(TwitterCredentialsOAuth2 credentials) {
-        TwitterOAuth20Service service = new TwitterOAuth20Service(
-                credentials.getTwitterOauth2ClientId(),
-                credentials.getTwitterOAuth2ClientSecret(),
-                "http://twitter.com",
-                "offline.access tweet.read users.read");
-
-        OAuth2AccessToken accessToken = null;
         try {
-            final Scanner in = new Scanner(System.in, "UTF-8");
-            System.out.println("Fetching the Authorization URL...");
+            // 1. get request token
+            RequestToken requestToken = twitterIns.getOAuthRequestToken();
+            String token = requestToken.getToken();
+            String tokenSecret = requestToken.getTokenSecret();
+            System.out.println("Token: " + token);
+            System.out.println("Token Secret: " + tokenSecret);
 
-            final String secretState = "state";
-            PKCE pkce = new PKCE();
-            pkce.setCodeChallenge("challenge");
-            pkce.setCodeChallengeMethod(PKCECodeChallengeMethod.PLAIN);
-            pkce.setCodeVerifier("challenge");
-            String authorizationUrl = service.getAuthorizationUrl(pkce, secretState);
+            // 2. build request path
+            HttpUrl.Builder urlBuilder = HttpUrl.parse(baseAuthUrl)
+                    .newBuilder();
+            urlBuilder.addPathSegment("authorize")
+                    .addQueryParameter("oauth_token", token)
+                    .addQueryParameter("oauth_token_secret", tokenSecret)
+                    .addQueryParameter("oauth_callback_confirmed", "true");
+            System.out.println("Authorize URL: " + urlBuilder.build());
 
-            System.out.println("Go to the Authorization URL and authorize your App:\n" +
-                    authorizationUrl + "\nAfter that paste the authorization code here\n>>");
-            final String code = in.nextLine();
-            System.out.println("\nTrading the Authorization Code for an Access Token...");
-            accessToken = service.getAccessToken(pkce, code);
-
-            System.out.println("Access token: " + accessToken.getAccessToken());
-            System.out.println("Refresh token: " + accessToken.getRefreshToken());
-        } catch (Exception e) {
-            System.err.println("Error while getting the access token:\n " + e);
+        } catch (TwitterException e) {
+            System.err.println("Status code: " + e.getStatusCode());
+            System.err.println("Error code: " + e.getErrorCode());
+            System.err.println("Error message: " + e.getErrorMessage());
             e.printStackTrace();
         }
-        return accessToken;
     }
 
+    @Test
+    public void oauth1_accessToken() {
+
+        String token = "5uIbXwAAAAABr1btAAABjTUUQl4";
+        String oauthVerifier = "4846211";
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(baseAuthUrl)
+                .newBuilder();
+        urlBuilder.addPathSegment("access_token")
+                .addQueryParameter("oauth_token", token)
+                .addQueryParameter("oauth_verifier", oauthVerifier);
+        HttpUrl url = urlBuilder.build();
+        System.out.println("url: " + url);
+
+        RequestBody requestBody = RequestBody.create(new byte[0], null);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            String respStr = response.body().string();
+            if (respStr.contains("&")) {
+                Map<String, String> params = parseStringToMap(respStr);
+                System.out.println("OAuth Token: " + params.get("oauth_token"));
+                System.out.println("OAuth Token Secret: " + params.get("oauth_token_secret"));
+                System.out.println("User ID: " + params.get("user_id"));
+                System.out.println("Screen Name: " + params.get("screen_name"));
+            } else {
+                System.out.println(respStr);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+    public static Map<String, String> parseStringToMap(String queryString) {
+        Map<String, String> params = new HashMap<>();
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            params.put(pair.substring(0, idx), pair.substring(idx + 1));
+        }
+        return params;
+    }
 }
